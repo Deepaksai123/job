4d71a05334dd8ec698d3e7c73e426b56f879b4bc
Deepaksai123 <deepakkota66@gmail.com>
2023-04-28 10:47:11 +0530

    add

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Hello world!
+ */
+class App {
+   
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
